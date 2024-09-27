17bb2c66f425f240028a584a6a1b4976ca333159
Ayan Panda <ayan.panda.inbox@gmail.com>
2024-09-28 01:07:08 +0530

    basic emement are added

---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export const Landing = () =>{
+    const [name, setName] = useState("");
+    return <div>
+        <input type="text" onChange={(e) => {
+            setName(e.target.value);
+        }}>
+        
+        </input>
+        <button onClick={() =>{
+            //join room logic
+        }}>Join
+
+        </button>
+    </div>
+}
